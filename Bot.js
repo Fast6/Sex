347bd20c8c7ccf25ee772fb2b233cd84b15976d7
Fast6 <45312443+Fast6@users.noreply.github.com>
2018-12-16 19:39:02 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', async() => {
 var server = "411591887384739841"; // ايدي السررفر
-var channel = "411591887384739845";//ايدي الروم
+var channel = "411591887384739843";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam-Spam **')
     },305);
